71b7e9b09d1c8108e466b4b3780f559f9ec53c25
David Tawfik <david.tawfik.intern@linkdevelopment.com>
2024-07-10 14:34:43 +0300

    Implemented IPCutter

---
+++ ip_cutter.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+void ipCutter(var ip) {
+  var splittedIp = ip.split('.');
+  for (var ip in splittedIp)
+  {
+    print(ip);
+  }
+}
+
+void main()
+{
+  var ip="163.121.12.30";
+  ipCutter(ip);
+}
